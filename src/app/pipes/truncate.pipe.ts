b15f507ba38fc9f5ec71c7ef7a1b65cff84e1291
Vassiliy Kryazhev <vassiliy.kryazhev@gmail.com>
2018-09-25 13:21:55 +0300

    I18n for history records.

--- src/app/pipes/truncate.pipe.ts
+++ src/app/pipes/truncate.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncatePipe implements PipeTransform {
 
   transform(value: any, size: number): any {
-    if (typeof value === 'string') {
+    if (typeof value === 'string' && value.length > size) {
       return value.substring(0, size) + '...';
     }
     return value;
